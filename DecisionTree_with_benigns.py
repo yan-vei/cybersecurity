8d15250800c20a556a85c99044b5f5c214d731a8
yan-vei <yana.veitsman@yandex.kz>
2021-05-19 10:02:03 +0200

    updates to code + tree

--- DecisionTree_with_benigns.py
+++ DecisionTree_with_benigns.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon May 17 21:04:22 2021
+Created on Tue May 18 22:00:53 2021
 
 @author: Yana
 """
@@ -10,7 +10,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import f1_score, precision_score, recall_score, accuracy_score
-from sklearn.preprocessing import MinMaxScaler  
+from sklearn.tree import DecisionTreeClassifier
 
 train = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
@@ -25,22 +25,16 @@ X_train = np.nan_to_num(X_train)
 X_test = test[test.columns[0:-1]].astype(dtype=np.float32).to_numpy()
 X_test = np.nan_to_num(X_test)
 
-# We can use preprocessing - MinMaxScaler - in particular to optimize the scales of the values of the features
-# of the train and test sets
-# scaler = MinMaxScaler()
-# X_train = scaler.fit_transform(X_train)
-# X_test = scaler.fit_transform(X_test)
-
 accuracies = []
 recalls = []
 precisions = []
 f1_scores = []
 
 for i in range(5):
-    clf = SVC(C=1, kernel='rbf', gamma='scale')
-    clf.fit(X_train, y_train)
-    y_pred = clf.predict(X_test)
-
+    tree = DecisionTreeClassifier()
+    tree.fit(X_train, y_train)
+    y_pred = tree.predict(X_test)
+    
     accuracies.append(accuracy_score(y_test, y_pred))
     recalls.append(recall_score(y_test, y_pred))
     precisions.append(precision_score(y_test, y_pred))
@@ -50,3 +44,5 @@ print("Mean accuracy: " + str(np.mean(accuracies)))
 print("Mean precision: " + str(np.mean(precisions)))
 print("Mean recalls: " + str(np.mean(recalls)))
 print("Mean F1-scores: " + str(np.mean(f1_scores)))
+
+
